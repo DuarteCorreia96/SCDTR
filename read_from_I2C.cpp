1bc4f29921aaaad52b168400195b9657c88c8292
Duarte Correia <duartecorreiascs@hotmail.com>
2018-11-22 21:46:19 +0000

    updated read_from_I2C

--- read_from_I2C.cpp
+++ read_from_I2C.cpp
@@ -72,10 +72,10 @@ int main(){
     bsc_xfer_t xfer;
     gpioInitialise();
 
-    //gpioSetMode(18, PI_ALT3);
-    //gpioSetMode(19, PI_ALT3);
+    gpioSetMode(18, PI_ALT3);
+    gpioSetMode(19, PI_ALT3);
 
-    xfer.control = (0x00 << 16) | 0x3FF; // Set I2C slave Address to 0x0A
+    xfer.control = (0x0A << 16) | 0x3FF; // Set I2C slave Address to 0x0A
 
     while (getchar() != 'q'){
 
